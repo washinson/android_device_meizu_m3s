e07b2bc304415c052c6f309ced0177df778535bc
washinson <sanya112001@yandex.ru>
2019-06-24 22:25:22 +0300

    add bluetooth default name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,12 +1,15 @@
 LOCAL_PATH := device/meizu/m3s
 
-# inherit from the proprietary version
+# Inherit from the proprietary version
 -include vendor/meizu/m3s/BoardConfigVendor.mk
 
 include device/meizu/m3_m3s-common/BoardConfigCommon.mk
 
-#prebuilt kernel image
+# Prebuilt kernel image
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
 
-#system.prop
+# system.prop
 TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
