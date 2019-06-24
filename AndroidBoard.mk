619a4f7a1554d3cb0ca2df3c47bee283a244aad3
washinson <sanya112001@yandex.ru>
2019-06-24 22:36:33 +0300

    add radio

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -6,3 +6,16 @@ ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
 -include vendor/meizu/m3s/AndroidBoardVendor.mk
+
+#----------------------------------------------------------------------
+# Radio image
+#----------------------------------------------------------------------
+
+ifeq ($(ADD_RADIO_FILES), true)
+radio_dir := $(LOCAL_PATH)/radio
+RADIO_FILES := $(shell cd $(radio_dir) ; ls)
+$(foreach f, $(RADIO_FILES), \
+    $(call add-radio-file,radio/$(f)))
+
+INSTALLED_RADIOIMAGE_TARGET += $(TARGET_BOOTLOADER_EMMC_INTERNAL)
+endif
